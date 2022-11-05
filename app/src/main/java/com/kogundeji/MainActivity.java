19fd54cedb1bdbff9d9f42b9266171ca3a017cf5
KOgundeji <97274909+KOgundeji@users.noreply.github.com>
2022-11-05 12:59:12 -0700

    More efficient use of layouts and resources

--- app/src/main/java/com/kogundeji/MainActivity.java
+++ app/src/main/java/com/kogundeji/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_layouts);
         spot_num = findViewById(R.id.spot_num);
         strike_num = findViewById(R.id.strike_num);
         vol_num = findViewById(R.id.vol_num);
